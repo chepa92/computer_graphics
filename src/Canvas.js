23c6edeffb8e44ad62da5486ab4bc6cd135f0948
chepa <chepa@bigeek.net>
2021-03-26 18:34:08 +0300

    draw pixel and line

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,28 +1,43 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const Canvas = (props) => {
   const canvasRef = useRef(null);
 
-  function drawPixel(context){
-    context.fillStyle = "#F00";
-    context.fillRect(15, 15, 1, 1); //x, y, heigh, width
-  }
-
-  function drawLine(context){
-    context.fillStyle = "#F00";
-    context.fillRect(15, 15, 1, 1); //x, y, heigh, width
-  }
-
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-
+    const context = canvasRef.current.getContext("2d");
     context.fillStyle = "#000000";
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
-    drawPixel(context);
-    drawLine(context);
+    putpixel(10, 15, "blue");
+    drawLine({x0: 1, y0: 1, x1: 200, y1: 10}, "red");
   }, []);
+
+  function putpixel(x, y, color) {
+    const context = canvasRef.current.getContext("2d");
+    context.fillStyle = color;
+    context.fillRect(x, y, 1, 1); //x, y, heigh, width
+  }
+
+  const drawLine = (position, color) => {
+    const { x0, y0, x1, y1 } = position;
+    let dx = x1 - x0;
+    let dy = y1 - y0;
+
+    let d = (2 * dy) - dx;
+    let y = y0;
+
+    for (let x = x0; x < x1; x++)
+    {
+      putpixel(x, y, color);
+        if (d >= 0)
+        {
+             y = y + 1;
+             d = d - 2 * dx;
+        }
+        d = d + 2 * dy;
+    }
+};
+  
 
   return <canvas ref={canvasRef} {...props} />;
 };
