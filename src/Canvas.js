ad56f155198473d4c43dd3f657fe564bc8cbf564
chepa <chepa@bigeek.net>
2021-03-26 19:00:38 +0300

    draw circle

--- src/Canvas.js
+++ src/Canvas.js
@@ -9,7 +9,8 @@ const Canvas = (props) => {
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
     putpixel(10, 15, "blue");
-    drawLine({x0: 1, y0: 1, x1: 200, y1: 10}, "red");
+    drawLine({ x0: 1, y0: 1, x1: 200, y1: 10 }, "red");
+    drawCircle(10, 50, 50, "green");
   }, []);
 
   function putpixel(x, y, color) {
@@ -19,25 +20,31 @@ const Canvas = (props) => {
   }
 
   const drawLine = (position, color) => {
+    //Bresenham's line algo
     const { x0, y0, x1, y1 } = position;
     let dx = x1 - x0;
     let dy = y1 - y0;
 
-    let d = (2 * dy) - dx;
+    let d = 2 * dy - dx;
     let y = y0;
 
-    for (let x = x0; x < x1; x++)
-    {
+    for (let x = x0; x < x1; x++) {
       putpixel(x, y, color);
-        if (d >= 0)
-        {
-             y = y + 1;
-             d = d - 2 * dx;
-        }
-        d = d + 2 * dy;
+      if (d >= 0) {
+        y = y + 1;
+        d = d - 2 * dx;
+      }
+      d = d + 2 * dy;
     }
-};
-  
+  };
+
+  const drawCircle = (radius, x, y, color) => {
+    for (var i = 0; i < 180; i++) {
+      let x1 = x + radius * Math.cos((2 * Math.PI * i) / 180);
+      let y2 = y + radius * Math.sin((2 * Math.PI * i) / 180);
+      putpixel(x1, y2, color);
+    }
+  };
 
   return <canvas ref={canvasRef} {...props} />;
 };
